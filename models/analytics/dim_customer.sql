27057c16f1c63a1787cba10b7cb748838b331141
thongdata <tanthong2206@gmail.com>
2023-11-05 22:38:57 +0700

    add dim_customer with cte structure

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -1,3 +1,24 @@
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.sales__customers`
+WITH dim_customer__source AS (
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.sales__customers`
+)
+
+, dim_customer__rename_column AS (
+  SELECT
+    customer_id AS customer_key
+    ,customer_name
+  FROM dim_customer__source
+)
+
+, dim_customer__cast_type AS (
+  SELECT
+    cast(customer_key as integer) as customer_key
+    ,cast(customer_name as string) as customer_name
+  FROM dim_customer__rename_column
+)
+
+SELECT
+  customer_key
+  ,customer_name
+FROM dim_customer__cast_type
