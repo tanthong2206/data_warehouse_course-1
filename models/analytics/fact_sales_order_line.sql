8efb7c1243a29795bc5900f2e67f935a0d54e644
thongdata <tanthong2206@gmail.com>
2023-11-05 22:04:03 +0700

    control data type

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,7 @@
 SELECT 
-  order_line_id as sales_order_line_key
-  ,stock_item_id as product_key
-  ,quantity
-  ,unit_price
-  ,quantity * unit_price as gross_amount
+  cast(order_line_id as integer) as sales_order_line_key
+  ,cast(stock_item_id as integer) as product_key
+  ,cast(quantity as integer) as quantity
+  ,cast(unit_price as numeric) as unit_price
+  ,cast(quantity as integer) * cast(unit_price as numeric) as gross_amount
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
