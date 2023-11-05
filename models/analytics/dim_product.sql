f2b1b946343d1ff24f6e3d8422f00227ab7b04d6
thongdata <tanthong2206@gmail.com>
2023-11-05 22:11:42 +0700

    cte rename column

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,17 @@
+WITH dimp_product_source AS (
+  SELECT 
+    *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+,dim_product_rename_column AS (
+  SELECT 
+     stock_item_id AS product_key
+    ,stock_item_name  AS product_name
+    ,brand AS brand_name
+  FROM dimp_product_source
+)
 SELECT 
-cast(stock_item_id as integer) as product_key
-,cast(stock_item_name as string) as product_name
-,cast(brand as string) as brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+   cast(product_key AS integer) AS product_key
+  ,cast(product_name AS string) AS product_name
+  ,cast(brand_name AS string) AS brand_name
+FROM dim_product_rename_column
