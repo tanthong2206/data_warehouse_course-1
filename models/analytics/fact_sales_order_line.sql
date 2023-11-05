817cb12a4aa5ebbbc3d6cdb8df65f66c3ba3ee4c
thongdata <tanthong2206@gmail.com>
2023-11-05 22:21:20 +0700

    cte cast type

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -9,14 +9,25 @@ SELECT
   order_line_id  AS sales_order_line_key
   ,stock_item_id  AS product_key
   ,quantity  AS quantity
-  ,unit_price as numeric) AS unit_price
+  ,unit_price  AS unit_price
   ,quantity  * unit_price  AS gross_amount
 FROM fact_sales_order_line__source
 )
+
+, fact_sales_order_line__cast_type AS (
+  SELECT 
+    cast(order_line_id as integer) AS sales_order_line_key
+    ,cast(stock_item_id as integer) AS product_key
+    ,cast(quantity as integer) AS quantity
+    ,cast(unit_price as numeric) AS unit_price
+    ,cast(quantity as integer) * cast(unit_price as numeric) AS gross_amount
+  FROM fact_sales_order_line__rename_column
+
+)
 SELECT 
-  cast(order_line_id as integer) as sales_order_line_key
-  ,cast(stock_item_id as integer) as product_key
-  ,cast(quantity as integer) as quantity
-  ,cast(unit_price as numeric) as unit_price
-  ,cast(quantity as integer) * cast(unit_price as numeric) as gross_amount
-FROM fact_sales_order_line__rename_column
+   sales_order_line_key
+  ,product_key
+  ,quantity
+  ,unit_price
+  ,gross_amount
+FROM fact_sales_order_line__cast_type
