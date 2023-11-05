138fd389d645059c4898502efbb930f87bfba992
thongdata <tanthong2206@gmail.com>
2023-11-05 22:15:45 +0700

    cte syntax restructure

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,16 +1,18 @@
-WITH dimp_product_source AS (
+WITH dimp_product__source AS (
   SELECT 
     *
   FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
 )
-,dim_product_rename_column AS (
+
+,dim_product__rename_column AS (
   SELECT 
      stock_item_id AS product_key
     ,stock_item_name  AS product_name
     ,brand AS brand_name
   FROM dimp_product_source
 )
-, dim_product_cast_type AS (
+
+, dim_product__cast_type AS (
   SELECT 
     cast(product_key AS integer) AS product_key
     ,cast(product_name AS string) AS product_name
