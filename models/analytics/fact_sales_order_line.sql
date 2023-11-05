472b2be5a689bbd49ee55052e5935aca318b5b1b
thongdata <tanthong2206@gmail.com>
2023-11-05 22:26:57 +0700

    cte calculated measure

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -20,14 +20,20 @@ FROM fact_sales_order_line__source
     ,cast(product_key as integer) AS product_key
     ,cast(quantity as integer) AS quantity
     ,cast(unit_price as numeric) AS unit_price
-    ,cast(quantity as integer) * cast(unit_price as numeric) AS gross_amount
   FROM fact_sales_order_line__rename_column
-
 )
+
+, fact_sales_order_line__calculated_measure AS (
+SELECT 
+   *
+  ,unit_price * quantity as gross_amount
+FROM fact_sales_order_line__cast_type
+)
+
 SELECT 
    sales_order_line_key
   ,product_key
   ,quantity
   ,unit_price
   ,gross_amount
-FROM fact_sales_order_line__cast_type
+FROM fact_sales_order_line__calculated_measure
