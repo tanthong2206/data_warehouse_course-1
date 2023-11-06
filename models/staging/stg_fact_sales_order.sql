f3972dd54619b274b91b9b68becc6e8eaebd7ec9
thongdata <tanthong2206@gmail.com>
2023-11-06 17:42:38 +0700

    rename fact table

--- models/staging/stg_fact_sales_order.sql
+++ models/staging/stg_fact_sales_order.sql
@@ -1,17 +1,17 @@
-WITH fact_sales_orders__source AS (
+WITH fact_sales_order__source AS (
   SELECT 
     *
   FROM `vit-lam-data.wide_world_importers.sales__orders`
 )
 
-, fact_sales_orders__rename_column AS (
+, fact_sales_order__rename_column AS (
 SELECT 
   order_id AS sales_order_key
   ,customer_id  AS customer_key
 FROM fact_sales_orders__source
 )
 
-, fact_sales_orders__cast_type AS (
+, fact_sales_order__cast_type AS (
   SELECT 
     cast(sales_order_key as integer) as sales_order_key
     ,cast(customer_key as integer) as customer_key
@@ -21,4 +21,4 @@ SELECT
   sales_order_key
   ,customer_key
  
-FROM fact_sales_orders__cast_type
+FROM fact_sales_order__cast_type
