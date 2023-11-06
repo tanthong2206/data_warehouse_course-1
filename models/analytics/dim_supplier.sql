0c8982d634afc324adef853c462c083ceab44731
thongdata <tanthong2206@gmail.com>
2023-11-06 22:15:29 +0700

    cte processing supplier table

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,24 @@
+WITH dim_supplier__source AS (
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+
+, dim_supplier__rename_column AS (
+  SELECT
+    supplier_id AS supplier_key
+    ,supplier_name AS supplier_name
+  FROM dim_supplier__source
+)
+
+, dim_supplier__cast_type AS (
+  SELECT
+    cast(supplier_key AS integer) AS supplier_key
+    ,cast(supplier_name as string) AS supplier_name
+  FROM dim_supplier__rename_column
+)
+
+SELECT
+  supplier_key
+  ,supplier_name
+FROM dim_supplier__cast_type
