1a8c0031a9abd3bc56545f7bc41f7ac08cbc2004
thongdata <tanthong2206@gmail.com>
2023-11-11 16:33:57 +0700

    add cte

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,3 +1,23 @@
+WITH dim_person__source AS (
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.application__people`
+)
+, dim_person__rename_column AS (
+  SELECT 
+    person_id AS person_key
+    ,full_name
+  FROM dim_person__source
+)
+
+, dim_person__cast_type AS (
+  SELECT
+    CAST(person_key AS integer) AS person_key
+    ,CAST(full_name AS string) AS full_name
+  FROM dim_person__rename_column
+)
+
+SELECT
+  persone_key
+  ,full_name
+FROM dim_person__cast_type
